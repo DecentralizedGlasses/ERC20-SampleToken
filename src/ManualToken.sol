f379f23cab2374eab42779ab13c89fea5121b486
sivaji <salla3@kent.edu>
2025-12-02 20:16:01 -0500

    Apply forge fmt

--- src/ManualToken.sol
+++ src/ManualToken.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.30;
 
 contract ManualToken {
     mapping(address => uint256) private s_balances;
+
     function name() public pure returns (string memory) {
         return "Manual Token";
     }
